19a697d00385a2dc7c3832fd4de055bd26c28543
Evan810828 <1015021084@qq.com>
2022-12-28 23:14:28 +0800

    fix

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import styled from 'style-components'
+
+const HeaderWrapper = styled.div`
+  back
+`;
+
+function name(params) {
+  return(
+    <HeaderWrapper className="Header">
+      ddddd
+    </HeaderWrapper>
+  )
+}
